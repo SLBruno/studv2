c7faf5060ae4fd3789ee9ea6532654a604c645d3
brunosl <brunolagoela@gmail.com>
2015-12-22 11:01:37 -0200

    0.055 - activeadmin working

---
+++ db/migrate/20151221143819_create_aulas.rb
@@ -0,0 +1,14 @@
+class CreateAulas < ActiveRecord::Migration
+  def change
+    create_table :aulas do |t|
+      t.string :title
+      t.text :content
+      t.string :video
+      t.boolean :section_header, :null => false, :default => false
+      t.string :tag
+      t.references :curso, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
