d429ef88ef12212520f929556b4be9c0d15c63e7
˜slbruno˜ <brunolagoela@gmail.com>
2016-12-07 23:48:20 -0200

    alteracao de migracao

--- db/migrate/20151230001711_add_color_to_curso.rb
+++ db/migrate/20151230001711_add_color_to_curso.rb
@@ -3,7 +3,6 @@ class AddColorToCurso < ActiveRecord::Migration
     add_column :cursos, :color, :string
     add_reference :cursos, :trilha, index: true
     add_foreign_key :cursos, :trilhas
-    add_reference :cursos, :subtrilha, index: true
     add_foreign_key :cursos, :subtrilhas
     add_column :cursos, :icon, :string
     add_column :cursos, :public, :text
