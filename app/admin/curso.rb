af847ba88dd3617192a0504eb4e7fffb66c4e6d7
˜slbruno˜ <brunolagoela@gmail.com>
2017-01-12 13:06:48 -0200

    atualizações gerais

--- app/admin/curso.rb
+++ app/admin/curso.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Curso do
-    permit_params :name, :content, :price, :image, :tag, :point, :color, :trilha_id, :icon, :public, :time, :profname, :proflink, :profdescription, :string, :profimage
+    permit_params :name, :content, :price, :image, :tag, :point, :color, :trilha_id, :icon, :public, :time, :profname, :proflink, :profdescription, :string, :profimage, :requeriment
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
